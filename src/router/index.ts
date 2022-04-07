e55824604f14ceac27468538fcdc71b7a14fa23a
YeMeiM <2324391514@qq.com>
2022-04-07 17:03:34 +0800

    添加路由守卫过滤

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,23 +2,48 @@ import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
 import {getAutoRoutes} from "./auto"
 import store from "@/store";
 
+// 路由地址配置
 const routes: Array<RouteRecordRaw> = [
   ...getAutoRoutes(),
 ]
 
+// 创建路由并保存路由对象
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
 
+// 路由拦截忽略
+const ignore = ["/", "/login/*"];
+
+/**
+ * 检测是否需要被忽略
+ * @param path 路径
+ */
+function isIgnore(path: string): boolean {
+  for (const p of ignore) {
+    if (p.endsWith("/*") && path.startsWith(p.substring(0, p.length - 2))) {
+      return true;
+    } else if (p === path) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// 设置路由守卫
 router.beforeEach((to, from, next) => {
-  if (to.path === "/" || to.path.startsWith("/login") || store.state.user) {
+  // 如果不开启路由收尾、已经登录或者是忽略的路由，则直接进入
+  if (process.env.VUE_APP_ROUTER_GUARD !== "true" || store.state.user || isIgnore(to.path)) {
+    to.meta.needLogin = false;
     next();
   } else {
-    next({
-      path: "/login",
-      replace: true,
-    })
+    to.meta.needLogin = true;
+    if (window.android) {
+      next();
+    } else {
+      next("/login")
+    }
   }
 })
 
